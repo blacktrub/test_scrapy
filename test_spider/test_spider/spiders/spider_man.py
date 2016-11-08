e937ebba8114c0960fb8730017ef3c9f97ee0854
BlackTrub <lightlocdog@gmail.com>
2016-11-08 20:59:47 +0300

    add scraping data;

--- test_spider/test_spider/spiders/spider_man.py
+++ test_spider/test_spider/spiders/spider_man.py
@@ -10,8 +10,20 @@ class SpiderManSpider(scrapy.Spider):
         ]
 
         for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse_links)
 
-    def parse(self, response):
+    def parse_links(self, response):
+        home_url = 'http://www.expoeast.com/ee16/public/'
         for link in response.xpath('//tr[@id="trnode"]/td/a').css('a.exhibitorName::attr(href)').extract():
-            yield {'link': link}
+            yield scrapy.Request(url=home_url+link, callback=self.parse_content)
+
+    def parse_content(self, response):
+        content = response.css('div.col-sm-8')
+        yield {
+            'name': content.css('h1::text').extract(),
+            'city': content.css('span.BoothContactCity::text').extract(),
+            'state': content.css('span.BoothContactState::text').extract(),
+            'zip': content.css('span.BoothContactZip::text').extract(),
+            'country': content.css('span.BoothContactCountry::text').extract(),
+
+        }
