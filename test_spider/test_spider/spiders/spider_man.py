001dc9ef77b015854ea19ae61ecdbd44fdc5aea3
BlackTrub <lightlocdog@gmail.com>
2016-11-08 20:17:50 +0300

    scraping links from prod site;

--- test_spider/test_spider/spiders/spider_man.py
+++ test_spider/test_spider/spiders/spider_man.py
@@ -6,17 +6,12 @@ class SpiderManSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
+            'http://www.expoeast.com/ee16/public/Exhibitors.aspx?Index=All&sortMenu=118003'
         ]
 
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split('/')[-2]
-        filename = 'quotes-%s.html' % page
-
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for link in response.xpath('//tr[@id="trnode"]/td/a').css('a.exhibitorName::attr(href)').extract():
+            yield {'link': link}
